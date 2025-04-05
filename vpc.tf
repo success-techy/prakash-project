3a6389296ac62442f70a4d905004216e66562f99
success-techy <127170918+success-techy@users.noreply.github.com>
2025-04-05 08:42:11 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -100,12 +100,6 @@ resource "aws_route_table" "private_rt" {
 
    vpc_id = aws_vpc.newvpc.id
 
-    route {
-   
-     cidr_block = "0.0.0.0/0"
-	 gateway_id = aws_nat_gateway.mynat.id
-	 
-	 }
    
    tags = {
 
